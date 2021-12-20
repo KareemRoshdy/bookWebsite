30e2434fafe38b3d6e3d1a47a6b2bedd79ee657a
KareemRoshdy <karem22roshdy@gmail.com>
2021-12-20 23:50:56 +0200

    book website

---
+++ js/script.js
@@ -0,0 +1,170 @@
+searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+}
+
+let loginForm = document.querySelector('.login-form-container');
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+}
+
+document.querySelector('#close-login-btn').onclick = () =>{
+    loginForm.classList.remove('active');
+}
+
+window.onscroll = () => {
+
+    searchForm.classList.remove('active');
+
+    if(window.scrollY > 80){
+        document.querySelector('.header .header-2').classList.add('active');
+    } else{
+        document.querySelector('.header .header-2').classList.remove('active');
+    }
+}
+window.onload = () => {
+
+    if(window.scrollY > 80){
+        document.querySelector('.header .header-2').classList.add('active');
+    } else{
+        document.querySelector('.header .header-2').classList.remove('active');
+    }
+
+    fadeOut();
+}
+
+
+function loader(){
+  document.querySelector('.loader-container').classList.add('active');
+}
+
+function fadeOut(){
+  setTimeout(loader,4000);
+}
+
+
+
+
+var swiper = new Swiper(".books-slider", {
+    loop:true,
+    centeredSlider: true,
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+var swiper = new Swiper(".featured-slider", {
+    spaceBetweem: 10,
+    loop:true,
+    centeredSlider: true,
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextE1: ".swiper-button-next",
+        prevE1: ".swiper-button-prev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      450: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1024: {
+        slidesPerView: 4,
+      },
+    },
+  });
+
+
+var swiper = new Swiper(".arrivals-slider", {
+    spaceBetweem: 10,
+    loop:true,
+    centeredSlider: true,
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+    },
+    
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+     
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+
+var swiper = new Swiper(".reviews-slider", {
+    spaceBetweem: 10,
+    grabCursor:true,
+    loop:true,
+    centeredSlider: true,
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+    },
+    
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+     
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+
+var swiper = new Swiper(".blogs-slider", {
+    spaceBetweem: 10,
+    grabCursor:true,
+    loop:true,
+    centeredSlider: true,
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+    },
+    
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+     
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
